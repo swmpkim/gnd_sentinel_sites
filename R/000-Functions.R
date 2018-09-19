c651f042ca398fd7a789c93ddc2517d420885e6c
swmpkim <kimberly.cressman@dmr.ms.gov>
2018-09-19 10:58:45 -0500

    updates to site names; addition of MH calculations

--- R/000-Functions.R
+++ R/000-Functions.R
@@ -13,11 +13,17 @@ attach_site_names <- function(dat, set_clm = "SET") {
 
     # set_clm defaults to being named "SET" - needs to be specified, in quotes, if it's something different
 
-    siteID <- data.frame("CLMA" = c("CLMAJ-1", "CLMAJ-2", "CLMAJ-3"),
-                         "PANN" = c("PANNE-1", "PANNE-2", "PANNE-3"),
-                         "JULO" = c("JURO-1", "JURO-2", "JURO-3"),
-                         "JUUP" = c("JURO-4", "JURO-5", "JURO-6"),
-                         "SPAL" = c("SPAL-1", "SPAL-2", "SPAL-3")) %>%
+    siteID <- data.frame("CLMAJ" = c("CLMAJ-1", "CLMAJ-2", "CLMAJ-3",
+                                     "CLMAJ_1", "CLMAJ_2", "CLMAJ_3"),
+                         "JURO_High" = c("PANNE-1", "PANNE-2", "PANNE-3",
+                                         "JURO_High_1", "JURO_High_2", 
+                                         "JURO_High_3"),
+                         "JURO_Low" = c("JURO-1", "JURO-2", "JURO-3",
+                                        "JURO_Low_1", "JURO_Low_2", "JURO_Low_3"),
+                         "JURO_Mid" = c("JURO-4", "JURO-5", "JURO-6", 
+                                        "JURO_Mid_1", "JURO_Mid_2", "JURO_Mid_3"),
+                         "SPAL" = c("SPAL-1", "SPAL-2", "SPAL-3",
+                                    "SPAL_1", "SPAL_2", "SPAL_3")) %>%
         tidyr::gather(key = "site", value = "SET") %>%
         dplyr::mutate(site = as.factor(site),
                SET = as.factor(SET))
